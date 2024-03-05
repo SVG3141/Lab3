993c161e2cef0ba5eeb1e1f0f04e8494dccafc2c
SVG3141 <95262250+SVG3141@users.noreply.github.com>
2024-03-05 18:51:35 -0500

    Update script-expressions.js
    updates scripts

--- script-expressions.js
+++ script-expressions.js
@@ -1,11 +1,3 @@
-/*--------------------------------------------------------------------
-GGR472 WEEK 6: JavaScript for Web Maps
-Using expressions to:
-    - filter data drawn in map layers
-    - define paint properties based on attribute data and zoom level
---------------------------------------------------------------------*/
-
-
 //Define access token
 mapboxgl.accessToken = 'pk.eyJ1IjoidmFsZGVzczAwNCIsImEiOiJjbHQ3bmE4b3QwbXhtMmptZzg1M3prMjhxIn0.cSHypfTdZswzMU2HDZGXVQ';
 
@@ -14,24 +6,21 @@ const map = new mapboxgl.Map({
     container: 'map', //container id in HTML
     style: 'mapbox://styles/mapbox/dark-v11',  //stylesheet location
     center: [-79.39, 43.65],  // starting point, longitude/latitude 43.652652, -79.393014
-    zoom: 12 // starting zoom level
+    zoom: 10, // starting zoom level
+
 });
-
-//Add zoom and rotation controls to the map.
-map.addControl(new mapboxgl.NavigationControl());
-
 
 map.on('load', () => {
 
     //Add datasource from GeoJSON
     map.addSource('toronto-rinks', {
         type: 'geojson',
-        data: 'https://github.com/SVG3141/Lab3/blob/main/data/indoor-ice-rinks-data.geojson'
-       
+        data: 'https://raw.githubusercontent.com/SVG3141/Lab3/main/data/indoor-ice-rinks-data.geojson'
+
     });
 
     map.addLayer({
-        'id': 'toronto-ice-rinks',
+        'id': 'toronto-rinks-pnts',
         'type': 'circle',
         'source': 'toronto-rinks',
         'paint': {
@@ -40,7 +29,7 @@ map.on('load', () => {
         }
     });
 
-    //Draw GeoJSON labels using 'name' property
+    // Draw GeoJSON labels using 'name' property
     map.addLayer({
         'id': 'toronto-rink-labels',
         'type': 'symbol',
@@ -58,74 +47,63 @@ map.on('load', () => {
 
 });
 
+//Add zoom and rotation controls to the map.
+map.addControl(
+    new MapboxGeocoder({
+        accessToken:mapboxgl.accessToken,
+        mapboxgl: mapboxgl,
+        countries: "ca"
 
-/*--------------------------------------------------------------------
-EXAMPLE FILTERS
-Data expressions: get, has
-Conditional expressions: ==, >=, (etc.), any, all
---------------------------------------------------------------------*/
-// //Filter data shown in layer
-// 'filter': ['>=', ['get', 'capacity'], 1000]  //Only shows points with capacity >= 1000
-// 'filter': ['==', ['get', 'name'], 'Horseshoe Tavern'] 
-// 'filter': ['has', 'opentimes']
-// 'filter': ['!', ['has', '...']]
+    })
+);
 
-// 'filter': ['any',    //ANY expression returns true if any inputs are met (OR)
-//     ['==', ['get', 'name'], 'Horseshoe Tavern'],
-//     ['==', ['get', 'name'], 'The Axis Club']] //returns features with name = "Horseshoe Tavern" or "The Axis Club"
+map.addControl(new mapboxgl.NavigationControl());
+map.addControl(new mapboxgl.FullscreenControl());
 
-// 'filter': ['all',    //ALL expression returns true if all inputs are met (AND)
-//     ['==', ['get', 'name'], 'Horseshoe Tavern'],
-//     ['==', ['get', 'name'], 'The Axis Club']] //returns features with name = "Horseshoe Tavern" or "The Axis Club"
+document.getElementById('returnbutton').addEventListener('click', () => {
+    map.flyTo({
+        center: [-79.39, 43.65],
+        zoom: 10,
+        essential: true
+    });
+});
 
+map.on('click', 'toronto-rinks-pnts', (e) => {
+    const coordinates = e.features[0].geometry.coordinates.slice();
+    const address = e.features[0].properties.Address;
+    new mapboxgl.Popup()
+        .setLngLat(coordinates)
+        .setHTML(address)
+        .addTo(map);
+});
 
+map.on('mouseenter', 'places', () => {
+    map.getCanvas().style.cursor = 'pointer';
+});
 
-/*--------------------------------------------------------------------
-EXAMPLE APPLICATION OF CATEGORICAL COLOUR SCHEME
-Data expressions: get
-Ramp/scale expression: step
---------------------------------------------------------------------*/
-//Changing colour of marker based on categories
-//Uses step and get expressions
-// [
-//     'step', // STEP expression produces stepped results based on value pairs
-//     ['get', 'capacity'], // GET expression retrieves property value from 'capacity' data field
-//     '#800026', // Colour assigned to any values < first step
-//     150, '#bd0026', // Colours assigned to values >= each step
-//     500, '#e31a1c',
-//     1000, '#fc4e2a',
-//     2500, '#fd8d3c'
-// ]
-
-
-
-/*--------------------------------------------------------------------
-EXAMPLE UPDATE OF MARKER SIZE BASED ON DATA VALUE AND ZOOM
-Data expressions: get
-Ramp/scale expression: interpolate (type: linear)
-Camera expression: zoom
-Maths expressions: /, *
---------------------------------------------------------------------*/
-//Set marker size to (capacity/20) using Maths expressions
-//['*', ['get', 'capacity'], 0.05]
-//['/', ['get', 'capacity'], 20]
-
-//Change marker size on zoom
-//Uses interpolate operator to define linear relationship between zoom level and circle size
-// [
-//     'interpolate', //INTERPOLATE expression produces continuous results by interpolating between value pairs
-//     ['linear'], //linear interpolation between stops but could be exponential ['exponential', base] where base controls rate at which output increases
-//     ['zoom'], //ZOOM expression changes appearance with zoom level
-//     8, 1, // when zoom level is 8 or less, circle radius will be 1px
-//     12, 10 // when zoom level is 12 or greater, circle radius will be 10px
-// ]
-
-// [
-//     'interpolate', //INTERPOLATE expression produces continuous results by interpolating between value pairs
-//     ['linear'], //linear interpolation between stops but could be exponential ['exponential', base] where base controls rate at which output increases
-//     ['zoom'], //zoom expression changes appearance with zoom level
-//     10, 5, // when zoom is 10 (or less), radius will be 5px
-//     12, ['/',['get', 'capacity'],20] // when zoom is 12 (or greater), radius will be capacity/20
-// ]
+// Change it back to a pointer when it leaves.
+map.on('mouseleave', 'places', () => {
+    map.getCanvas().style.cursor = '';
+});
 
 
+let communityvalue;
+
+document.getElementById("communityfieldset").addEventListener('change',(e) => {   
+    communityvalue = document.getElementById('boundary').value;
+
+    console.log(communityvalue); // Useful for testing whether correct values are returned from dropdown selection
+
+    if (communityvalue == 'All') {
+        map.setFilter(
+            'toronto-rinks-pnts',
+            ['has', 'Community Council Area'] // Returns all points from layer that have a value in field
+        );
+    } else {
+        map.setFilter(
+            'toronto-rinks-pnts',
+            ['==', ['get', 'Community Council Area'], communityvalue] // returns points with value that matches dropdown selection
+        );
+    }
+
+});
